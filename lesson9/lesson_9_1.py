3cc86eb4cb0fd5f8d0628211c956073c7befcff4
Yosya Izmaylov <yosyaismaylov@gmail.com>
2022-11-17 15:38:48 +0200

    done homework 9_1

--- lesson9/lesson_9_1.py
+++ lesson9/lesson_9_1.py
@@ -1,1 +1,23 @@
-#1
+class MyAuto:
+    def __init__(self, car_model ='', colour = '', vehicle = 0):
+        self.car_model = car_model
+        self.colour = colour
+        self.vehicle = vehicle
+
+    def go_vpered(self):
+        return print(f"{self.colour} {self.car_model} V{self.vehicle} їде вперед")
+    def go_nazad(self):
+        return print(f"{self.colour} {self.car_model} V{self.vehicle} їде назад")
+
+class MyAuto2(MyAuto):
+
+    def go_right(self):
+        return print(f"{self.colour} {self.car_model} V{self.vehicle} їде направо")
+    def go_left(self):
+        return print(f"{self.colour} {self.car_model} V{self.vehicle} їде наліво")
+
+car = MyAuto2("Renault Clio", "Сіре", 1.4)
+car.go_vpered()
+car.go_nazad()
+car.go_right()
+car.go_left()
