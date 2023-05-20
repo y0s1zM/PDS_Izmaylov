a61cd9d490eccb72a835e00f381b7dc9e54d2259
Yosya Izmaylov <yosyaismaylov@gmail.com>
2023-05-20 18:44:52 +0300

    zadachi z lekcii #4 dodano

---
+++ lesson2/zadachi z lekcii/5.py
@@ -0,0 +1,5 @@
+seconds = 12
+minutes = 35 * 60
+hours = 17 * 60 ** 2
+days = 13 * 24 * 60 ** 2
+print(f"За 13 днів, 17 годин, 35 хвилин і 12 секунд у світі народилось {seconds + minutes + hours + days} дітей ")
