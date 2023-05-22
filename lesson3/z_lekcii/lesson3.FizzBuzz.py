3384d4a0a335f8e5048023ae6d96a88ec7b7970e
Yosya Izmaylov <yosyaismaylov@gmail.com>
2023-05-22 23:48:12 +0300

    lesson3FizzBuzz

---
+++ lesson3/z_lekcii/lesson3.FizzBuzz.py
@@ -0,0 +1,13 @@
+i = 1
+while i <= 100:
+    if i % 15 == 0:
+        print("FizzBuzz")
+    elif i % 5 == 0:
+        print("Buzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    else:
+        print(i)
+
+
+    i = i + 1
