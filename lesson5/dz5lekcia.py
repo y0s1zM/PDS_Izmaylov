2b3946b9ea611309a14f01eafe18f8360403c9ef
Yosya Izmaylov <yosyaismaylov@gmail.com>
2023-06-05 16:01:01 +0300

    dz5lekcia.py #2

--- lesson5/dz5lekcia.py
+++ lesson5/dz5lekcia.py
@@ -1,23 +1,23 @@
-    #1
-# def last_lines(path):
-#     with open(path, mode= 'r') as file:
-#         rows = 0
-#         ml = []
-#         for f in file:
-#             ml.append(f)
-#             rows += 1
-#     print(ml[rows - 2])
-#     print(ml[rows - 1])
-#
-# last_lines('test.txt')
+    # 1
+def last_lines(path):
+    with open(path, mode= 'r') as file:
+        rows = 0
+        ml = []
+        for f in file:
+            ml.append(f)
+            rows += 1
+    print(ml[rows - 2])
+    print(ml[rows - 1])
 
-    #2
+last_lines('test.txt')
 
-# numbers = list(range(101))
-# list = [num for num in numbers if num % 5 == 0]
-# def new_file_function(list):
-#     with open("new_file.txt", mode='w') as file:
-#         file.write(fr"{list}")
-#     return
-#
-# new_file_function(list)
+    # 2
+
+numbers = list(range(101))
+list = [num for num in numbers if num % 5 == 0]
+def new_file_function(list):
+    with open("new_file.txt", mode='w') as file:
+        file.write(fr"{list}")
+    return
+
+new_file_function(list)
