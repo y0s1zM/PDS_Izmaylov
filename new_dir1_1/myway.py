b34b3dff948245674f8e2497f656ace327816af7
y0s1zM <yosyaismaylov@gmail.com>
2024-12-25 13:45:27 +0200

    new file

---
+++ new_dir1_1/myway.py
@@ -0,0 +1,7 @@
+print ("It is a new project by y0s1k!")
+
+first_number = input("Input first number")
+second_number = input("Input second number")
+dobavlenie = first_number + second_number
+
+print(dobavlenie)
